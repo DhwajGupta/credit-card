b7c608920405ffb92f15f13983530d86363c7b31
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-08-31 14:49:18 +0530

    Add snackbar display on submission

--- src/containers/Card/index.jsx
+++ src/containers/Card/index.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react'
 import CardDisplay from '../../components/CardDisplay/index'
 import CardForm from '../../components/CardForm/index'
 import Box from '@material-ui/core/Box'
+import Snackbar from '@material-ui/core/Snackbar'
+import MuiAlert from '@material-ui/lab/Alert'
 import useStyles from './Card.styles'
 import { DEFAULT_CARD_TYPE } from '../../utils/constants'
+
+const Alert = props => {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const Card = () => {
   const classes = useStyles()
   const [fieldToFocus, setFieldToFocus] = useState(null)
+  const [openSnackbar, setOpenSnackbar] = useState(false)
+  const [snackbarMessage, setSnackbarMessage] = useState({ type: 'info', message: '' })
   const [cardDetails, setCardDetails] = useState({
     cvv: '',
     month: '',
@@ -39,9 +47,34 @@ const Card = () => {
     }))
   }
 
+  const handleSnackbarClose = () => {
+    setOpenSnackbar(false)
+    setSnackbarMessage({ type: 'info', message: '' })
+  }
+
+  const checkForEmptyFields = () => {
+    // eslint-disable-next-line no-unused-vars
+    for (const [_, value] of Object.entries(cardDetails)) {
+      if (!value) return true
+    }
+    return false
+  }
+
   const handleSubmitCardDetails = (e) => {
     e.preventDefault()
     setFieldToFocus(null)
+    if(checkForEmptyFields()) {
+      setSnackbarMessage({
+        type: 'error',
+        message: 'Fill all the fields.'
+      })
+    } else {
+      setSnackbarMessage({
+        type: 'success',
+        message: 'Submitted successfully.'
+      })
+    }
+    setOpenSnackbar(true)
   }
 
   return (
@@ -58,6 +91,16 @@ const Card = () => {
         handleFieldFocus={handleFieldFocus}
         handleSubmit={handleSubmitCardDetails}
       />
+      <Snackbar 
+        open={openSnackbar} 
+        autoHideDuration={3000}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        onClose={handleSnackbarClose}
+      >
+        <Alert severity={snackbarMessage.type}>
+          {snackbarMessage.message}
+        </Alert>
+      </Snackbar>
     </Box>
   )
 }
