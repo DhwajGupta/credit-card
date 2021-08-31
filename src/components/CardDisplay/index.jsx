e26a7d82849fe0da5d3eb17b4c452d2de40873d1
Dhwaj Gupta <dhwajgupta27@gmail.com>
2021-08-31 13:07:04 +0530

    Add logic for background change on flip

--- src/components/CardDisplay/index.jsx
+++ src/components/CardDisplay/index.jsx
@@ -10,11 +10,22 @@ import useStyles from './CardDisplay.styles'
 const CardDisplay = (props) => {
   const { cardDetails, fieldToFocus, handleCardType } = props
   const classes = useStyles()
+  const [ isFlipped, setFlip ] = useState(false)
   const [ background, setBackground ] = useState('/images/6.jpeg')
+
+  useEffect(() => {
+    if (fieldToFocus === 'cvv') {
+      setFlip(true)
+      setBackground('/images/7.jpeg')
+    } else {
+      setFlip(false)
+      setBackground('/images/6.jpeg')
+    }
+  }, [fieldToFocus === 'cvv'])
 
   return (
     <Box className={ classes.cardDisplay } style={{ backgroundImage: `url(${background})` }}>
-      <ReactCardFlip isFlipped={fieldToFocus === 'cvv'} flipDirection='horizontal'>
+      <ReactCardFlip isFlipped={isFlipped} flipDirection='horizontal'>
         <CardFrontDisplay
           cardDetails={cardDetails}
           fieldToFocus={fieldToFocus}
